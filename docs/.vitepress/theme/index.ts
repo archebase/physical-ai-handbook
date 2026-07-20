2cfd014a3986497f987e7ee7769ad4b6fb022458
Zhexuan Yang <zhexuany@gmail.com>
2026-07-20 09:44:04 +0800

    perf(search): preload the local search index

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,24 @@ import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 import MermaidDiagram from './MermaidDiagram.vue';
 
+function preloadSearchIndex() {
+  if (typeof window === 'undefined') return;
+
+  const preload = () => {
+    // VitePress generates this virtual module during the production build.
+    // @ts-expect-error virtual module provided by VitePress local search
+    import('@localSearchIndex')
+      .then(({default: indexes}) => indexes.root?.())
+      .catch(() => {});
+  };
+
+  window.setTimeout(preload, 1000);
+}
+
 export default {
   extends: DefaultTheme,
-  enhanceApp({app}) { app.component('MermaidDiagram', MermaidDiagram); }
+  enhanceApp({app}) {
+    app.component('MermaidDiagram', MermaidDiagram);
+    preloadSearchIndex();
+  }
 };
